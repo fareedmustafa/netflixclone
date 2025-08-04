dceec85428853f76f17a4d2de235e1b269ed25b6
Mohammed Fareed Mustafa <168615588+fareedmustafa@users.noreply.github.com>
2025-08-03 22:25:24 -0400

    db fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:5000", // Changed from localhost to 127.0.0.1
+        changeOrigin: true,
+      },
+    },
+  },
 });
